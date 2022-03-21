f1e857ae9a2c21d3023f418ff9ce567dad094ca2
Gaf513 <68122636+Gaf513@users.noreply.github.com>
2022-03-21 08:55:47 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,87 @@
-let House = sprites.create(img`
-    ....................8a8aa8a8....................
-    .................aaa888aa8a8aaa.................
-    ..............aaa8aa8a8aa888aa8aaa..............
-    ...........8aa8aa8888a8aa8a8888aa8aa8...........
-    ........8888aa8aa8aa8a8aa8a8aa8aa8aa8888........
-    .....aaa8aa8aa8888aa8a8aa8a8aa8888aa8aa8aaa.....
-    ...aa8888aa8aa8aa8aa888aa888aa8aa8aa8aa8888aa...
-    dccaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aaccd
-    bcb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bcb
-    dbbaa8aa8888aa8aa8888a8aa8a8888aa8aa8888aa8aabbd
-    dbbaa8aa8aa8aa8888aa8a8aa8a8aa8888aa8aa8aa8aabbd
-    dccaa8888aa8aa8aa8aa888aa888aa8aa8aa8aa8888aaccd
-    bcbaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aabcb
-    dbb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bbd
-    dbbaa8aa8888aa8aa8aa8a8aa8a8aa8aa8aa8888aa8aabbd
-    dccaa8aa8aa8aa8aa8888a8aa8a8888aa8aa8aa8aa8aaccd
-    bcbaa8888aa8aa8888aa888aa888aa8888aa8aa8888aabcb
-    dbbaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aabbd
-    dbb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bbd
-    dccaa8aa8888aa8aa8aa8a8aa8a8aa8aa8aa8888aa8aaccd
-    bcbaa8aa8aa8aa8aa8aa888aa888aa8aa8aa8aa8aa8aabcb
-    dbbaa8888aa8aa8aa888ccbbbbcc888aa8aa8aa8888aabbd
-    dbbaa8aa8aa8aa888ccbbbbbbbbbbcc888aa8aa8aa8aabbd
-    dcc888aa8aa888ccbbbbbccccccbbbbbcc888aa8aa888ccd
-    bcbaa8aa888ccbbbbbccbddddddbccbbbbbcc888aa8aabcb
-    dbbaa8aaccbbbbbccbddddddddddddbccbbbbbccaa8aabbd
-    dbbaaccbbbbcccbddddddddddddddddddbcccbbbbccaabbd
-    dcccbbbbcccbdddbccbbbbbbbbbbbbccbdddbcccbbbbcccd
-    ccccccccbbbbbbbcbddddddddddddddbcbbbbbbbcccccccc
-    bddddddddddddbcddddddddddddddddddcbddddddddddddb
-    bbcbdddddddddcbd1111111111111111dbcdddddddddbcbb
-    bbbcccccccccccd1bbbbbbbbbbbbbbbb1dcccccccccccbbb
-    bbbbdddddddddc11beeeeeeeeeeeeeeb11cdddddddddbbbb
-    bbb8aaaaaaa8dc1be3b33b33b33b33beb1cd8aaaaaaa8bbb
-    bbb888888888dc1be3b33b33b33b33beb1cd888888888bbb
-    bbb833333338dcbbf3b3effffffe33bebbcd833333338bbb
-    bbb83ff3ff38dcbbf3bffffffffff3bebbcd83ff3ff38bbb
-    bbb83cc3cc38dcbbf3effffffffffebebbcd83cc3cc38bbb
-    bbb833333338dcbbf3eeeeeeeeeeeebebbcd833333338bbb
-    cbb83ff3ff38dcbbe3b33b33b33b33bebbcd83ff3ff38bbc
-    cbb83cc3cc38dcbbe3b33b33b33b33bebbcd83cc3cc38bbc
-    ccbbbbbbbbbbdcbbe3b33b33b33feeeebbcdbbbbbbbbbbcc
-    .cbbdddddddddcbbe3b33b33b33ffffebbcdddddddddbbc.
-    ..cbdbbbdbbbdcbbf3b33b33b33f33febbcdbbbdbbbdbc..
-    ...cdbbbdbbbdcbbf3b33b33b33bffeebbcdbbbdbbbdc...
-    ....bddddddddcbbf3b33b33b33b33bebbcddddddddb....
-    .....bdbbbdddcbbf3b33b33b33b33bebbcdddbbbdb.....
-    ......bcccbbbcbbe3b33b33b33b33bebbcbbbcccb......
-    `, SpriteKind.Player)
-tiles.placeOnTile(House, tiles.getTileLocation(3, 2))
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+namespace SpriteKind {
+    export const Tower = SpriteKind.create()
+}
+function setHome () {
+    House = sprites.create(img`
+        ....................8a8aa8a8....................
+        .................aaa888aa8a8aaa.................
+        ..............aaa8aa8a8aa888aa8aaa..............
+        ...........8aa8aa8888a8aa8a8888aa8aa8...........
+        ........8888aa8aa8aa8a8aa8a8aa8aa8aa8888........
+        .....aaa8aa8aa8888aa8a8aa8a8aa8888aa8aa8aaa.....
+        ...aa8888aa8aa8aa8aa888aa888aa8aa8aa8aa8888aa...
+        dccaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aaccd
+        bcb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bcb
+        dbbaa8aa8888aa8aa8888a8aa8a8888aa8aa8888aa8aabbd
+        dbbaa8aa8aa8aa8888aa8a8aa8a8aa8888aa8aa8aa8aabbd
+        dccaa8888aa8aa8aa8aa888aa888aa8aa8aa8aa8888aaccd
+        bcbaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aabcb
+        dbb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bbd
+        dbbaa8aa8888aa8aa8aa8a8aa8a8aa8aa8aa8888aa8aabbd
+        dccaa8aa8aa8aa8aa8888a8aa8a8888aa8aa8aa8aa8aaccd
+        bcbaa8888aa8aa8888aa888aa888aa8888aa8aa8888aabcb
+        dbbaa8aa8aa8888aa8aa8a8aa8a8aa8aa8888aa8aa8aabbd
+        dbb888aa8aa8aa8aa8aa8a8aa8a8aa8aa8aa8aa8aa888bbd
+        dccaa8aa8888aa8aa8aa8a8aa8a8aa8aa8aa8888aa8aaccd
+        bcbaa8aa8aa8aa8aa8aa888aa888aa8aa8aa8aa8aa8aabcb
+        dbbaa8888aa8aa8aa888ccbbbbcc888aa8aa8aa8888aabbd
+        dbbaa8aa8aa8aa888ccbbbbbbbbbbcc888aa8aa8aa8aabbd
+        dcc888aa8aa888ccbbbbbccccccbbbbbcc888aa8aa888ccd
+        bcbaa8aa888ccbbbbbccbddddddbccbbbbbcc888aa8aabcb
+        dbbaa8aaccbbbbbccbddddddddddddbccbbbbbccaa8aabbd
+        dbbaaccbbbbcccbddddddddddddddddddbcccbbbbccaabbd
+        dcccbbbbcccbdddbccbbbbbbbbbbbbccbdddbcccbbbbcccd
+        ccccccccbbbbbbbcbddddddddddddddbcbbbbbbbcccccccc
+        bddddddddddddbcddddddddddddddddddcbddddddddddddb
+        bbcbdddddddddcbd1111111111111111dbcdddddddddbcbb
+        bbbcccccccccccd1bbbbbbbbbbbbbbbb1dcccccccccccbbb
+        bbbbdddddddddc11beeeeeeeeeeeeeeb11cdddddddddbbbb
+        bbb8aaaaaaa8dc1be3b33b33b33b33beb1cd8aaaaaaa8bbb
+        bbb888888888dc1be3b33b33b33b33beb1cd888888888bbb
+        bbb833333338dcbbf3b3effffffe33bebbcd833333338bbb
+        bbb83ff3ff38dcbbf3bffffffffff3bebbcd83ff3ff38bbb
+        bbb83cc3cc38dcbbf3effffffffffebebbcd83cc3cc38bbb
+        bbb833333338dcbbf3eeeeeeeeeeeebebbcd833333338bbb
+        cbb83ff3ff38dcbbe3b33b33b33b33bebbcd83ff3ff38bbc
+        cbb83cc3cc38dcbbe3b33b33b33b33bebbcd83cc3cc38bbc
+        ccbbbbbbbbbbdcbbe3b33b33b33feeeebbcdbbbbbbbbbbcc
+        .cbbdddddddddcbbe3b33b33b33ffffebbcdddddddddbbc.
+        ..cbdbbbdbbbdcbbf3b33b33b33f33febbcdbbbdbbbdbc..
+        ...cdbbbdbbbdcbbf3b33b33b33bffeebbcdbbbdbbbdc...
+        ....bddddddddcbbf3b33b33b33b33bebbcddddddddb....
+        .....bdbbbdddcbbf3b33b33b33b33bebbcdddbbbdb.....
+        ......bcccbbbcbbe3b33b33b33b33bebbcbbbcccb......
+        `, SpriteKind.Tower)
+    tiles.placeOnTile(House, tiles.getTileLocation(3, 2))
+    statusbar = statusbars.create(40, 5, StatusBarKind.Health)
+    statusbar.attachToSprite(House)
+    statusbar.setBarBorder(1, 15)
+}
+let statusbar: StatusBarSprite = null
+let House: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
+setHome()
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
