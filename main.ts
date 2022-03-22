ee4e497aecd72b7b492e4161d3f1b35dcc6e0aac
OwenDonaher <101876428+OwenDonaher@users.noreply.github.com>
2022-03-22 10:05:51 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,72 @@ namespace SpriteKind {
     export const Tower = SpriteKind.create()
     export const info = SpriteKind.create()
 }
+let spawnNumber = 0
+function enemySpawn (level: number) {
+    levelText = textsprite.create("Level" + level)
+    for (let index = 0; index <= spawnNumber; index++) {
+        enemySnake = sprites.create(img`
+            . . . . . c c c c c c c . . . . 
+            . . . . c 6 7 7 7 7 7 6 c . . . 
+            . . . c 7 c 6 6 6 6 c 7 6 c . . 
+            . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+            . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+            . . f 7 8 1 f f 1 6 7 7 7 f . . 
+            . . f 6 f 1 f f 1 f 7 7 7 f . . 
+            . . . f f 2 2 2 2 f 7 7 6 f . . 
+            . . c c f 2 2 2 2 7 7 6 f c . . 
+            . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+            c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+            f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+            f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+            f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+            . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+            . . f f c c c c c c c c . . . . 
+            `, SpriteKind.Player)
+        enemySnake.follow(House, 25)
+        if (index == 0) {
+            enemyBat = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . c c . . c c . . . . . . . . 
+                . . c 3 c c 3 c c c . . . . . . 
+                . c b 3 b c 3 b c c c . . . . . 
+                . c b b b b b b b b f f . . . . 
+                c c b b b b b b b b f f . . . . 
+                c b 1 b b b 1 b b c f f f . . . 
+                c b b b b b b b b f f f f . . . 
+                f b c b b b c b c c b b b . . . 
+                f b 1 f f f 1 b f c c c c . . . 
+                . f b b b b b b f b b c c . . . 
+                c c f b b b b b c c b b c . . . 
+                c c c f f f f f f c c b b c . . 
+                . c c c . . . . . . c c c c c . 
+                . . c c c . . . . . . . c c c c 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Enemy)
+            enemyBat.follow(House, 50)
+        } else {
+            enemyDinosaur = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . c c c c . . . . . . . . 
+                . . c c 5 5 5 5 c c . . . . . . 
+                . c 5 5 5 5 5 5 5 5 c . . . . . 
+                c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+                c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+                c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+                c c b b 1 b 5 5 5 5 5 5 d c . . 
+                c 5 3 3 3 5 5 5 5 5 d d d c . . 
+                . b 5 5 5 5 5 5 5 5 d d d c . . 
+                . . c b b c 5 5 b d d d d c . . 
+                . c b b c 5 5 b b d d d d c c c 
+                . c c c c c c d d d d d d d d c 
+                . . . c c c c d 5 5 b d d c c . 
+                . . c b b c c c 5 5 b c c . . . 
+                . . c c c c c d 5 5 c . . . . . 
+                `, SpriteKind.Enemy)
+            enemyDinosaur.follow(House, 20)
+        }
+    }
+}
 function instructions () {
     game.splash("Welcome to Defend the Home! Your goal to to protect your home from invaders hell bent on destroying it. Press A to learn how to play.")
     game.splash("Use the arrow keys to move around to hunt down and catch you enemies")
@@ -22,31 +88,6 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         }
     }
 })
-function forLater () {
-    tiles.setCurrentTilemap(tilemap`level1`)
-    setHome()
-    mySprite = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    scene.cameraFollowSprite(mySprite)
-    mySprite.setPosition(55, 65)
-}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (gameMenuOn == true) {
         gameMenuOn = false
@@ -166,6 +207,29 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         hardButton.destroy()
         impossibleButton.destroy()
         instructions()
+        tiles.setCurrentTilemap(tilemap`level1`)
+        setHome()
+        mySprite = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        scene.cameraFollowSprite(mySprite)
+        mySprite.setPosition(55, 65)
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -255,15 +319,19 @@ function setHome () {
     statusbar.setBarBorder(1, 15)
 }
 let statusbar: StatusBarSprite = null
-let House: Sprite = null
+let mySprite: Sprite = null
 let impossibleButton: TextSprite = null
 let hardButton: TextSprite = null
 let mediumButton: TextSprite = null
 let easyButton: TextSprite = null
 let difficultyMenuOn = false
-let mySprite: Sprite = null
 let levelType = ""
 let selector: Sprite = null
+let enemyDinosaur: Sprite = null
+let enemyBat: Sprite = null
+let House: Sprite = null
+let enemySnake: Sprite = null
+let levelText: TextSprite = null
 let selecting = false
 let gameMenuOn = false
 let pressAStart: TextSprite = null
